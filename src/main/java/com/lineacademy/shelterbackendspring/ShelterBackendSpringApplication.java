221494474a9fb922f1167dabfa48f24701624923
molang404 <art104233@naver.com>
2026-09-01 14:55:10 +0900

    초기화

---
+++ src/main/java/com/lineacademy/shelterbackendspring/ShelterBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.lineacademy.shelterbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lineacademy/shelterbackendspring/ShelterBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineacademy.shelterbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
